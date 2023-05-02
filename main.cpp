63b66a547cdec7ea02e036716273a548679f7849
Snocci <128066678+Snocci@users.noreply.github.com>
2023-05-02 21:05:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() {int lista[10];
+     for(int i=0;i<10;i++){
+     cin >> lista[i];
+     }
+     int a=0;
+     cin >> a;
+     int b, c, x;
+     for (int i = 0; i <10; i++) {
+         if (a <= lista[i]) {
+             b = lista[i];
+             lista[i]=a;
+             for (i = i + 1; i < 10; i++) {
+                c=lista[i];
+                x=c;
+                lista[i]=b;
+                b=x;
+
+             }
+
+         }
+
+     }
+     for (int i=0;i<10;i++) {
+
+         cout<<lista[i]<< " ";
+
+        }
    return 0;
 }
